5fb446880a7a8ad588adeb7bd3c813f49e67eae2
junhyuk <dejavukk1@gmail.com>
2019-11-15 23:13:48 +0900

    다양한 AlertAction추가 구현

--- Message-AlertController/ViewController.swift
+++ Message-AlertController/ViewController.swift
@@ -9,11 +9,39 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // Alert결과 알려주는 레이블.
+    @IBOutlet var result: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    
+    @IBAction func alertButton(_ sender: UIButton) {
+        
+        // 메시지창 객체 생성
+        let alert = UIAlertController(title: "선택", message: "항목을 선택해주세요.", preferredStyle: .alert)
+        let cancel = UIAlertAction(title: "취소", style: .cancel)
+        
+        // 확인 버튼 추가.
+        let ok = UIAlertAction(title: "확인", style: .default)
+        let exec = UIAlertAction(title: "실행", style: .destructive)
+        let stop = UIAlertAction(title: "중지", style: .default)
+        
+        // 버튼을 컨트롤러에 등록
+        alert.addAction(cancel)
+        alert.addAction(ok)
+        alert.addAction(exec)
+        alert.addAction(stop)
+        
+        // 메시지 창 실행
+        self.present(alert, animated: false)
+        
+        print("메시지 얼럿창 구현 성공.")
+    }
+    
 
 
 }
