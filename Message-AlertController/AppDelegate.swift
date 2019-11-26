ff77b33742867ae2ffff7c2c20d6aaeda654472e
junhyuk <dejavukk1@gmail.com>
2019-11-26 23:50:50 +0900

    Notification(알림) 구현.

--- Message-AlertController/AppDelegate.swift
+++ Message-AlertController/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import UserNotifications
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,6 +16,18 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        if #available(iOS 11.0, *) {
+            // 경고창, 배지, 사운드를 사용하는 알림 환경 정보를, 생성하고, 사용자 동의 여부 차을 실행
+            let notiCenter = UNUserNotificationCenter.current()
+            notiCenter.requestAuthorization(options: [.alert, .badge, .sound]) { (didAllow, e) in }
+        } else {
+            
+            let setting = UIUserNotificationSettings(types: [.alert, .badge, .sound], categories: nil)
+            application.registerUserNotificationSettings(setting)
+        }
+        
+        
         return true
     }
 
