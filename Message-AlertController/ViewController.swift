cf82b1df801c28eb80491666b8b72293f4e6a0d0
junhyuk <dejavukk1@gmail.com>
2019-12-01 23:01:58 +0900

    DatePicker, 관련 메소드 추가, 프로퍼티 선언, DispatchQueue.main.async...

--- Message-AlertController/ViewController.swift
+++ Message-AlertController/ViewController.swift
@@ -7,11 +7,18 @@
 //
 
 import UIKit
+import UserNotifications
 
 class ViewController: UIViewController {
     
     // Alert결과 알려주는 레이블.
     @IBOutlet var result: UILabel!
+    
+    // TextField, DatePicker 프로퍼티 선언.
+    @IBOutlet weak var msg: UITextField!
+    @IBOutlet weak var datePicker: UIDatePicker!
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,6 +30,54 @@ class ViewController: UIViewController {
         // 메시지 창을 처리하기 위한 적절한 위치
         
     }
+    
+    // 저장 메소드 선언.
+    @IBAction func save(_ sender: UIButton) {
+        
+        if #available(iOS 10, *) {
+            
+            // UserNotification 프레임워크를 사용한 로컬 알림
+            // 알림 동의 여부를 확인
+            UNUserNotificationCenter.current().getNotificationSettings { settings in
+                if settings.authorizationStatus == UNAuthorizationStatus.authorized {
+                    // 알림 설정이 들어갈 곳
+                    
+                    DispatchQueue.main.async {
+                        // 알림 콘텐츠 정의
+                        let nContent = UNMutableNotificationContent()
+                        nContent.body = (self.msg.text)!
+                        nContent.title = "미리 알림"
+                        nContent.sound = UNNotificationSound.default
+                        
+                        // 발송 시각을 '지금으로부터 *초 형식으로 변환
+                        let time = self.datePicker.date.timeIntervalSinceNow
+                        // 발송 조건 정의
+                        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: time, repeats: false)
+                        // 발송 요청 객체 정의
+                        let request = UNNotificationRequest(identifier: "알람", content: nContent, trigger: trigger)
+                        
+                        // 노티피케이션 센터에 추가
+                        UNUserNotificationCenter.current().add(request)
+                    }
+                    
+                    
+                } else {
+                    let alert = UIAlertController(title: "알림 등록", message: "알림이 허용되어 있지 않습니다.", preferredStyle: .alert)
+                    let ok = UIAlertAction(title: "확인", style: .default)
+                    alert.addAction(ok)
+                    
+                    self.present(alert, animated: false)
+                    return
+                }
+            }
+        } else {
+            
+            // LocalNotification 객체를 사용한 로컬 알림
+        }
+        
+        
+    }
+    
     
     
     @IBAction func alertButton(_ sender: UIButton) {
